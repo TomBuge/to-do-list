c9d333750356bacb4495b699be63adacbcee7774
Tom Bugeja <46567954+TomBuge@users.noreply.github.com.>
2026-01-21 11:41:53 +0000

    Create header nav

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import "./styles.css"; 
-import loadHomePage from "./home.js";
-import loadMenuPage from "./menu.js";
-import loadAboutPage from "./about.js";
-
-loadHomePage();
-
-console.log("We're connected and running!");
-
-
-const div = document.querySelector('.nav');
-div.addEventListener('click', (e) =>{
-    const button = e.target.id;
-
-    if (button === "menu") loadMenuPage();
-    if (button === "home") loadHomePage();
-    if (button === "about") loadAboutPage();
-});
+import './styles.css';
